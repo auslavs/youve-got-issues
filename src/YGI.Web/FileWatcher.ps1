aeef4ae8f3081f9823f1a20b11d344e6fd4ca31a
Rob Slavin <Rob.Slavin@dematic.com>
2020-04-30 16:49:08 +1000

    Changed to upload to local blob emulator

--- src/YGI.Web/FileWatcher.ps1
+++ src/YGI.Web/FileWatcher.ps1
@@ -1,18 +1,60 @@
 $watcher = New-Object System.IO.FileSystemWatcher
 $watcher.Path = Get-Location
-$watcher.IncludeSubdirectories = $false
+$watcher.IncludeSubdirectories = $true
 $watcher.EnableRaisingEvents = $true
+
+function Store-Blob
+{
+    param(
+        [Parameter(ValueFromPipeline)]
+        [string] $sourceFilePath,
+        [string] $destination,
+        [string] $contentType
+    )
+  
+    write-host "Copying File to local blob emulator: $sourceFilePath"
+    $ctx = New-AzStorageContext -Local
+    $container = $ctx | Get-AzStorageContainer -Name site
+
+    Set-AzStorageBlobContent -File $sourceFilePath `
+        -Container $container.Name `
+        -Blob $destination `
+        -Context $ctx `
+        -Properties @{"ContentType" = $contentType;} `
+        -Force
+
+}
 
 $changed = Register-ObjectEvent $watcher "Changed" -Action {
 
    [System.IO.FileInfo]$file = $eventArgs.FullPath
+   [string]$filepath = $eventArgs.FullPath
+   [string]$folder = $watcher.Path
+   [string]$blobpath = $filepath.Replace("$folder\","")
 
-   if ($file.Extension -eq ".html")
-   {
-        write-host "Copying File: $($file.FullName)"
-        $newlocation = "$($file.DirectoryName)\bin\Debug\netcoreapp3.1\$($file.Name)"
-        write-host "Destination: $newlocation"
-        copy-item $file.FullName -Destination $newlocation -Force
 
-   }
+    switch($file.Extension) {
+
+        ".html" { 
+            Store-Blob `
+            -sourceFilePath $filepath `
+            -destination $blobpath `
+            -contentType "text/html; charset=utf-8"
+        }
+
+        ".css" { 
+            Store-Blob `
+            -sourceFilePath $filepath `
+            -destination $blobpath `
+            -contentType "text/css; charset=utf-8"
+        }
+        ".js" { 
+            Store-Blob `
+            -sourceFilePath $filepath `
+            -destination $blobpath `
+            -contentType "text/javascript; charset=utf-8"
+        }
+
+    }
+
 }
