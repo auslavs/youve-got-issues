e76edd2dbc85fa7a26f4cf6be5a927b5625b8c34
Rob Slavin <Rob.Slavin@dematic.com>
2020-05-11 17:26:44 +1000

    Start on testing

--- test/YGI.Core.Test/TestHelpers.fs
+++ test/YGI.Core.Test/TestHelpers.fs
@@ -1,8 +1,16 @@
 ﻿module TestHelpers
 
+  open System
+
   let unpackResult result =
     match result with
     | Ok x -> x
     | Error err -> failwithf "%A" err
 
   let listEquals a b = List.fold (&&) true (List.zip a b |> List.map (fun (aa,bb) -> aa=bb))
+
+  let ranStr n = 
+    let r = Random()
+    let chars = Array.concat([[|'a' .. 'z'|];[|'A' .. 'Z'|];[|'0' .. '9'|]])
+    let sz = Array.length chars in
+    String(Array.init n (fun _ -> chars.[r.Next sz]))
--- test/YGI.Core.Test/Tests.fs
+++ test/YGI.Core.Test/Tests.fs
@@ -1,4 +1,4 @@
-module IssueUpdates
+module Issues.Updates
 
 open System
 open Xunit
@@ -17,6 +17,15 @@ let description = String500.create "Description" "Description" |> unpackResult
 let raisedBy    = String100.create "RaisedBy" "RaisedBy" |> unpackResult
 let date        = DateTime.Parse("12/10/2019")
 
+let TitlesEqual       (o:Issue,u:Issue) = Assert.Equal(o.Title, u.Title); (o,u)
+let DescriptionsEqual (o:Issue,u:Issue) = Assert.Equal(o.Description, u.Description); (o,u)
+let AreasEqual        (o:Issue,u:Issue) = Assert.Equal(o.Area, u.Area); (o,u)
+let EquipmentsEqual   (o:Issue,u:Issue) = Assert.Equal(o.Equipment, u.Equipment); (o,u)
+let IssueTypesEqual   (o:Issue,u:Issue) = Assert.Equal(o.IssueType, u.IssueType); (o,u)
+let RaisedBysEqual    (o:Issue,u:Issue) = Assert.Equal(o.RaisedBy, u.RaisedBy); (o,u)
+let CommentsEqual     (o:Issue,u:Issue) = Assert.True(listEquals o.Comments u.Comments); (o,u)
+
+
 
 /// Base issue we will use to test our updates
 let issue = 
@@ -29,6 +38,7 @@ let issue =
     Description  = description
     Comments     = []
     Resolution   = None
+    Attachments  = []
     RaisedBy     = raisedBy
     Status       = Status.Unopened
     Raised       = date
@@ -36,6 +46,90 @@ let issue =
     DateClosed   = None
     Vid          = guid
   }
+
+[<Fact>]
+let ``Update Title`` () =
+  
+  let newdata      = ranStr 100
+  let titleUpdated = String100.create "Title" newdata |> unpackResult
+  
+  let update : IssueUpdate =
+    {
+      ItemNo      = 1
+      Title       = titleUpdated
+      Description = description
+      Area        = area
+      Equipment   = equipment
+      IssueType   = issueType
+      Status      = Status.Unopened
+      Vid         = guid
+    }
+  
+  let result = Issue.update issue update
+  
+  let updatedIssue = 
+    match result with
+    | Ok i -> i
+    | Error err -> failwithf "%A" err
+  
+  /// Check appropriate fields have been updated
+  Assert.NotEqual(issue.Vid, updatedIssue.Vid)
+  Assert.NotEqual(issue.Title, updatedIssue.Title)
+  Assert.Equal(updatedIssue.Title, titleUpdated)
+  Assert.True (updatedIssue.LastChanged > issue.LastChanged)
+  
+  /// Check appropriate fields remain the same
+
+  Assert.Equal(issue.Description, updatedIssue.Description)
+  Assert.Equal(issue.Area, updatedIssue.Area)
+  Assert.Equal(issue.Equipment, updatedIssue.Equipment)
+  Assert.Equal(issue.IssueType, updatedIssue.IssueType)
+  Assert.True(listEquals issue.Comments updatedIssue.Comments)
+  Assert.Equal(issue.Resolution, updatedIssue.Resolution)
+  Assert.Equal(issue.Raised, updatedIssue.Raised)
+  Assert.Equal(issue.RaisedBy, updatedIssue.RaisedBy)
+  Assert.Equal(issue.Status, updatedIssue.Status)
+  Assert.Equal(issue.DateClosed, updatedIssue.DateClosed)
+
+[<Fact>]
+let ``Update Description`` () =
+  
+  let newdata = ranStr 500
+  let descriptionUpdated = String500.create "Description" newdata |> unpackResult
+  
+  let update : IssueUpdate =
+    {
+      ItemNo      = 1
+      Title       = title
+      Description = descriptionUpdated
+      Area        = area
+      Equipment   = equipment
+      IssueType   = issueType
+      Status      = Status.Unopened
+      Vid         = guid
+    }
+  
+  let result = Issue.update issue update
+  
+  let updatedIssue = 
+    match result with
+    | Ok i -> i
+    | Error err -> failwithf "%A" err
+
+ 
+  /// Check appropriate fields have been updated
+  Assert.NotEqual(issue.Vid, updatedIssue.Vid)
+  Assert.NotEqual(issue.Description, updatedIssue.Description)
+  Assert.Equal(updatedIssue.Description, descriptionUpdated)
+  Assert.True (updatedIssue.LastChanged > issue.LastChanged)
+  
+  /// Check appropriate fields remain the same
+  (issue, updatedIssue)
+  |> TitlesEqual
+  |> AreasEqual
+  |> IssueTypesEqual
+  |> CommentsEqual
+  |> ignore
 
 [<Fact>]
 let ``Update Area`` () =
